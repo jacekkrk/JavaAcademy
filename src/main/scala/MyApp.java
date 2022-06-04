f8a40b1a92522e1341f7763bb4304f599a877870
KubaG <jacek.krk@gmail.com>
2022-06-04 16:05:18 +0200

    Add Application

---
+++ src/main/scala/MyApp.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+
+public class MyApp {
+
+    static String arg;
+
+    public static void main(String[] args) {
+
+        String parameterKey = "JavaAcademy";
+        int countResult = 0;
+
+        if (args.length != 0) {
+            arg = args[0];
+
+            if (arg.equals(parameterKey) == true) {
+
+                arg = args[0].toLowerCase(Locale.ROOT);
+
+                for (int i = 0; i < arg.length(); i++) {
+                    if (arg.charAt(i) == 'j' || arg.charAt(i) == 'v' || arg.charAt(i) == 'c' || arg.charAt(i) == 'd') {
+
+
+                        countResult += 1;
+                    } else if (arg.charAt(i) == 'm' || arg.charAt(i) == 'y' || arg.charAt(i) == 'p') {
+                        countResult += 2;
+                    } else if (arg.charAt(i) == 'a') {
+                        countResult += 5;
+                    }
+
+                }
+
+                System.out.println("Count letters " + countResult);
+            } else
+            {
+                System.out.println("Incorrect parameter");
+            }
+
+        }
+else
+        {
+            System.out.println("Run aplication with parameter");
+
+        }
+    }
+
+}
